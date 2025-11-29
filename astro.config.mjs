4285b681fd8106d43f412c9e71cfd766229022b0
MqyGalaxy <2412083655@qq.com>
2025-11-29 16:20:57 +0800

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mqygalaxy.github.io',
-  base: '/Aya-s-Silent-Homeland-Wiki',
+  site: 'https://aya.moeqy.com',
 })
